d587d34821091e6a3705ea4056b46de4fad61d0d
rebeccabuie <rebeccabuie@gmail.com>
2018-07-12 13:32:35 -0600

    added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  
+  root to: 'pages#home'
 end
